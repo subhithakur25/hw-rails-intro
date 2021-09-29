6029ef6690ce73254bb8e5daea5e8f5fa09ace99
EC2 Default User <ec2-user@ip-172-31-47-224.us-east-2.compute.internal>
2021-09-29 05:33:12 +0000

    add class method in Movie model for database query

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,7 @@ class Movie < ActiveRecord::Base
     def self.all_ratings
         all_ratings = ['G', 'PG', "PG-13", 'R']
     end
+    def self.with_ratings(ratings)
+        Movie.where(:rating => ratings)
+    end    
 end
